b4eeaefe94ee7bd2da1101a673b25a3ce09c4c32
Facundo López Llopis <faculopllop@gmail.com>
2024-05-13 21:29:40 -0300

    fix: log creator

--- game/utils/logsUtils.py
+++ game/utils/logsUtils.py
@@ -4,17 +4,19 @@ from models.enviroment import Enviroment
 
 from stable_baselines3.common.vec_env import DummyVecEnv
 
-def createLogFile(algorithm, env: DummyVecEnv, timesteps, obsDescription):
+def createLogFile(algorithm, envWrap: DummyVecEnv, timesteps, obsDescription):
     logpath = "./Logs/"
+    env = envWrap.envs[0].unwrapped
     newest = max([f for f in os.listdir(logpath)], key=lambda x: os.stat(os.path.join(logpath,x)).st_birthtime)
     f = open(logpath + newest + "/info.txt","w+")
-    f.writelines([
+    lines = [
         "Date: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
         "Algorithm: " + algorithm,
         "Timesteps (Matches): " + str(timesteps),
-        "Decks: " + str(env.envs[0].ndecks),
-        "Rewards (Win, Lose, Tie): " + str(env.winrew) + " " + str(env.envs[0].loserew) + " 0",
-        "Ignored rounds: " + str(env.envs[0].ignoredRounds),
+        "Decks: " + str(env.ndecks),
+        "Rewards (Win, Lose, Tie): " + str(env.winrew) + " " + str(env.loserew) + " 0",
+        "Ignored cards: " + str(env.ignoredCards),
         "Observation space: " + obsDescription
-    ])
+    ]
+    f.writelines(line + '\n' for line in lines)
     f.close()
